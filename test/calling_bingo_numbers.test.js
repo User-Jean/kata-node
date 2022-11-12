4fc4e91c59a11dcca65b67d7071da581de056252
jrojavid2 <jrojavid2@outlook.es>
2022-11-12 16:01:50 -0500

    Fixed test Calling Bingo Numbers

--- test/calling_bingo_numbers.test.js
+++ test/calling_bingo_numbers.test.js
@@ -2,12 +2,11 @@ import { callingBingoNumbers } from "../src/calling_bingo_numbers";
 
 test('Test calling Bingo Numbers', () => {
   const mySet = new Set();
-  [...Array(74)].forEach(_ => {
+  [...Array(75)].forEach(_ => {
 
     let number = callingBingoNumbers();
-    console.log(number)
-    expect(callingBingoNumbers()).toBeGreaterThanOrEqual(1)
-    expect(callingBingoNumbers()).toBeLessThanOrEqual(75);
+    expect(number).toBeGreaterThanOrEqual(1)
+    expect(number).toBeLessThanOrEqual(75);
     mySet.add(number);
 
   });
