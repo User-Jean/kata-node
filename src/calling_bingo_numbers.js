fc15ed16b2e331a462469df5a42222679b260494
jrojavid2 <jrojavid2@outlook.es>
2022-11-12 16:02:39 -0500

    First feature completed

--- src/calling_bingo_numbers.js
+++ src/calling_bingo_numbers.js
@@ -1,5 +1,18 @@
-const numbers = [];
+const numbers = {};
+
+// Validate the number exist
+function numberExists(number) {
+  if(numbers[number]) return true;
+  return false;
+}
 
 export function callingBingoNumbers() {
-  return 0;
+  // number random between 1 - 75
+  let number;
+  do {
+    number = Math.floor(Math.random() * 75 + 1)
+  } while(numberExists(number))
+  numbers[number] = true;
+
+  return number;
 }
